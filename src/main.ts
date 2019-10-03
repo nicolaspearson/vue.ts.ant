59f59e631e0e29c39daa1e640168d30d39e75f8d
Nic Pearson <npearson@minddoc.de>
2019-10-03 11:47:29 +0200

    Updated Deps

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+import Vue, { CreateElement } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,5 +9,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h: CreateElement) => h(App),
 }).$mount('#app');
